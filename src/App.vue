18363c3f7aefcec3f107036062218cb35a43d4d4
Fidelcastillo <ikevin2000@hotmail.com>
2025-09-15 21:58:20 -0600

    responsive

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,48 @@
 import Header from './components/header.vue';
 import HeroSecction from './components/HeroSecction.vue';
 import Sections from './components/Sections.vue';
+import { ref, onMounted, computed, onUnmounted } from 'vue';
+
+
+// calculamos el alto de la pantalla para ajustar el header
+const HeaderRef = ref<InstanceType<typeof Header> | null>(null);
+const HeroSectionRef = ref<InstanceType<typeof HeroSecction> | null>(null);
+const headerHeight = ref(0);
+
+//estilos dinamicos para el header
+const headerStyles = computed(() => ({
+  'height': `calc(100vh - ${headerHeight.value}px)`,
+  'margin-top': `${headerHeight.value}px`
+}));
+
+const calculateHeaderHeight = () => {
+  if (HeaderRef.value) {
+    headerHeight.value = HeaderRef.value.$el.offsetHeight;
+  }
+};
+onMounted(() => {
+  calculateHeaderHeight();
+  window.addEventListener('resize', calculateHeaderHeight);
+});
+onUnmounted(() => {
+  window.removeEventListener('resize', calculateHeaderHeight);
+});
+  
 </script>
 
 <template>
-  <Header />
-  <HeroSecction />
+  <Header ref="HeaderRef" class="header"  />
+  <HeroSecction ref="HeroSectionRef" :style="headerStyles" />
   <Sections />  
 </template>
 
 <style scoped>
 /* header always on top */
-header {
+.header {
   position: fixed;
   top: 0;
-  z-index: 10;
+  left: 0;
+  z-index: 50;
   width: 100%;
 }
 </style>
--- src/components/herotemp.vue
+++ src/components/herotemp.vue
@@ -1,23 +1,19 @@
 <template>
-    <section class="flex flex-col justify-center items-center text-center text-[var(--text)] p-4 parallax">
+        <section class="flex flex-col justify-center items-center text-center text-[var(--text)] p-4 parallax">
         <!-- El contenedor principal del contenido -->
         <div class="bg-white shadow-lg items-center justify-center rounded-lg 
-            flex flex-col md:flex-row gap-4 p-6 py-6 md:p-8
-            xl:h-fit xl:w-3/5
-            md:h-fit md:w-8/10
-            sm:h-8/10 sm:w-6/10
-            h-fit w-full"
-            >
+            flex flex-col md:flex-row gap-4 p-6 py-6 md:p-8  
+            ">
             <!-- Contenedor de la imagen -->
-            <div class="flex-1 flex h-fit justify-center items-center">
+            <div class="flex h-fit justify-center items-center">
                 <img 
                     src="/Erika.jpg" 
                     alt="Foto de Erika" 
-                    class="object-contain w-56 md:w-64 xl:w-full xl:h-full" 
+                    class="object-contain w-full " 
                 />
             </div>
             <!-- Contenedor del texto -->
-            <div class="flex flex-col flex-1 justify-center text-[var(--primary)] text-left gap-1 md:gap-4 px-4 h-full">
+            <div class="flex flex-col  text-[var(--primary)] text-left gap-1 md:gap-4 px-4 h-fit">
                 <h1 class="text-xl md:text-4xl font-bold mb-0">Erika Díaz Villalón</h1>
                 <p class="text-lg md:text-3xl text-[var(--text)]">Abogada</p>
                 <p class="text-2xl md:text-5xl font-bold">Líder social</p>
@@ -26,23 +22,15 @@
                     Las cosas lindas que me están pasando las acepto y agradezco con amor
                 </p>
                 <!-- Contenedor de los íconos de redes sociales -->
-                <div class="flex justify-start items-center gap-4 m-1 text-center">
+                <div class="flex justify-start items-center gap-4 text-center">
                     <font-awesome-icon icon="fa-brands fa-square-facebook" size="2x" class="text-[var(--primary)] hover:text-[var(--accent)] cursor-pointer transition duration-300 ease-linear" />
                     <font-awesome-icon icon="fa-brands fa-instagram" size="2x" class="text-[var(--primary)] hover:text-[var(--accent)] cursor-pointer transition duration-300 ease-linear" />
                     <font-awesome-icon icon="fa-brands fa-x-twitter" size="2x" class="text-[var(--primary)] hover:text-[var(--accent)] cursor-pointer transition duration-300 ease-linear" />
                 </div>
-                <p class="text-base md:text-2xl text-[var(--text)] underline font-inter italic cursor-pointer hover:text-[var(--accent)] transition duration-300 ease-linear">
+                <p class="text-sm md:text-2xl text-[var(--text)] underline font-inter italic cursor-pointer hover:text-[var(--accent)] transition duration-300 ease-linear">
                     Acerca de mi
                 </p>    
             </div>
         </div>
     </section>
-</template> 
-<style scoped>
-.parallax {
-    background-image: url('/Carretera.jpg');
-    background-attachment: fixed;
-    background-size: cover;
-    background-position: center;
-}
-</style>
+</template>
